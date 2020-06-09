4e526770907230329104830d56d37db54d144a2e
Olli Warro <olli.warro@gmail.com>
2020-06-09 18:48:00 +0300

    make button work + level0

---
+++ Hmmm/Assets/HandleLevelPressScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandleLevelPressScript : MonoBehaviour
+{
+    public string identifier;
+
+    public void OnPress() {
+        SceneManager.LoadScene(identifier, LoadSceneMode.Single);
+    }
+}
