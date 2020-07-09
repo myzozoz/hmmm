717cfbac04fa449a0c10099bf94b4de26291fa30
Olli Warro <olli.warro@gmail.com>
2020-07-09 11:18:22 +0300

    ground highlight when tapped on

--- Hmmm/Assets/Scripts/Input/inputController.cs
+++ Hmmm/Assets/Scripts/Input/inputController.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 public class inputController : MonoBehaviour
 {
 
+    public GameObject pointer;
     private ActionTarget currentTarget = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointer.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,52 +22,53 @@ public class inputController : MonoBehaviour
         {
             if (touch.phase == TouchPhase.Began)
             {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, Physics.DefaultRaycastLayers))
-                {
-                    Debug.Log("HIT " + hit.collider.name);
-                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.green, 3);
-
-                    if (hit.collider.tag == "Interactable")
-                    {
-                        if (currentTarget != null)
-                        {
-                            currentTarget.DisableSelectionIndicator();
-                        }
-                        if (GameObject.ReferenceEquals(hit.collider.gameObject.GetComponent<ActionTarget>(), currentTarget))
-                        {
-                            currentTarget.DisableSelectionIndicator();
-                            currentTarget = null;
-                        }
-                        else
-                        {
-                            // Highlight
-                            currentTarget = hit.collider.gameObject.GetComponent<ActionTarget>();
-                            currentTarget.EnableSelectionIndicator();
-                            // Open action menu
-                        }
-                    }
-                }
+                HandleInput(touch.position);
             }
         }
 
-        /*
+        
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            HandleInput(Input.mousePosition);
+        }
+    }
+
+    void HandleInput(Vector3 position) {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, Physics.DefaultRaycastLayers))
+        {
+            Debug.Log("HIT " + hit.collider.name);
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.green, 3);
+            string tag = hit.collider.tag;
+
+            if (tag == "Interactable")
             {
-                if (hit.collider.tag  == "Interactable")
+                if (currentTarget != null)
                 {
-                    Debug.Log("HIT Interactable: " + hit.collider.name);
+                    currentTarget.DisableSelectionIndicator();
+                }
+                if (GameObject.ReferenceEquals(hit.collider.gameObject.GetComponent<ActionTarget>(), currentTarget))
+                {
+                    currentTarget.DisableSelectionIndicator();
+                    currentTarget = null;
                 }
                 else
                 {
-                    Debug.Log("Hit something else " + hit.collider.name);
+                    // Highlight
+                    currentTarget = hit.collider.gameObject.GetComponent<ActionTarget>();
+                    currentTarget.EnableSelectionIndicator();
+                    // Open action menu
                 }
             }
-        }*/
+            else if (tag == "Ground") {
+                Vector3 targetPosition = hit.point;
+                targetPosition.x = Mathf.Round(targetPosition.x);
+                targetPosition.y = 0.1f;
+                targetPosition.z = Mathf.Round(targetPosition.z);
+                pointer.transform.position = targetPosition;
+                pointer.SetActive(true);
+            }
+        }
     }
 }
